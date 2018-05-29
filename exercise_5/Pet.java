45b97afd5b95d46cc14827d9a54bc70619e5d96e
mzamanAtgsu <mzaman@gsu.edu>
2018-05-29 15:51:08 -0400

    renamed folder

---
+++ exercise_5/Pet.java
@@ -0,0 +1,16 @@
+package com.test.tea.leaves;
+
+public abstract class Pet{
+	
+	private String name;
+	
+	public Pet(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public abstract String speak();
+}
